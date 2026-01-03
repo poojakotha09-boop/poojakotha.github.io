13d13a0d0d5a1f477f81fcf77b94c4a2a7043fd9
poojakotha09-boop <poojakotha09@gmail.com>
2026-01-03 10:22:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Dark Mode
+document.getElementById("themeToggle").onclick = () => {
+  document.body.classList.toggle("dark");
+};
+
+// Scroll Reveal
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
