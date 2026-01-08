34a677086258e09764862c0688eb545281c92a2c
poojakotha09-boop <poojakotha09@gmail.com>
2026-01-08 22:15:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -109,19 +109,3 @@ function bindUIEvents() {
   });
 }
 
-/* =========================================
-   PAGE STATE DETECTION
-   Shows problem-solving skill
-========================================= */
-
-document.addEventListener("DOMContentLoaded", () => {
-  const body = document.body;
-  const page = window.location.pathname.split("/").pop();
-
-  // Default = Home page
-  if (page === "" || page === "index.html") {
-    body.classList.add("page-home");
-  } else {
-    body.classList.add("page-inner");
-  }
-});
